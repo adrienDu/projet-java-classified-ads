c5ba335e65f69e2edf2cc35a3916b4df902a928a
MoazChaudhry <moaz.chaudry@hotmail.com>
2016-12-02 10:30:04 +0100

    Add files via upload
    Ajout de permission et premium

---
+++ Classified-Model/src/com/ecetech/bti4/itproject/classified/beans/Permission.java
@@ -0,0 +1,26 @@
+package com.ecetech.bti4.itproject.classifield.beans;
+
+public class Permission {
+
+	
+	private String idPermission;
+	
+	public Permission(String idPermission) {
+		super();
+		this.idPermission = idPermission;
+	}
+
+	public Permission() {
+		// TODO Auto-generated constructor stub
+		this("");
+	}
+
+	public String getIdPermission() {
+		return idPermission;
+	}
+
+	public void setIdPermission(String idPermission) {
+		this.idPermission = idPermission;
+	}
+
+}
---
+++ Classified-Model/src/com/ecetech/bti4/itproject/classified/beans/Premium.java
@@ -0,0 +1,92 @@
+package com.ecetech.bti4.itproject.classifield.beans;
+
+import java.util.Date;
+
+public class Premium {
+	
+
+	private int idPremium;
+	private Date datePayPremium;
+	private int dureePremium;
+	private int montantPayPremium;
+	private int id_User;
+	
+	
+
+
+	public Premium(int idPremium, Date datePayPremium, int dureePremium, int montantPayPremium, int id_User) {
+		super();
+		this.idPremium = idPremium;
+		this.datePayPremium = datePayPremium;
+		this.dureePremium = dureePremium;
+		this.montantPayPremium = montantPayPremium;
+		this.id_User = id_User;
+	}
+	
+	
+	public Premium() {
+		
+	}
+	
+
+
+
+	public int getIdPremium() {
+		return idPremium;
+	}
+
+
+
+	public void setIdPremium(int idPremium) {
+		this.idPremium = idPremium;
+	}
+
+
+
+	public Date getDatePayPremium() {
+		return datePayPremium;
+	}
+
+
+
+	public void setDatePayPremium(Date datePayPremium) {
+		this.datePayPremium = datePayPremium;
+	}
+
+
+
+	public int getDureePremium() {
+		return dureePremium;
+	}
+
+
+
+	public void setDureePremium(int dureePremium) {
+		this.dureePremium = dureePremium;
+	}
+
+
+
+	public int getMontantPayPremium() {
+		return montantPayPremium;
+	}
+
+
+
+	public void setMontantPayPremium(int montantPayPremium) {
+		this.montantPayPremium = montantPayPremium;
+	}
+
+
+
+	public int getId_User() {
+		return id_User;
+	}
+
+
+
+	public void setId_User(int id_User) {
+		this.id_User = id_User;
+	}
+
+}
