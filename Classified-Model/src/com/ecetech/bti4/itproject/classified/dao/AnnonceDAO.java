5c4770979f764b5379afefde19e4b2bfe4acf090
adrienDu <adrien.duhoux@gmail.com>
2016-12-02 11:49:21 +0100

    AnnonceDAO

--- Classified-Model/src/com/ecetech/bti4/itproject/classified/dao/AnnonceDAO.java
+++ Classified-Model/src/com/ecetech/bti4/itproject/classified/dao/AnnonceDAO.java
@@ -1,5 +1,32 @@
 package com.ecetech.bti4.itproject.classified.dao;
 
-public class AnnonceDAO {
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import com.ecetech.bti4.itproject.classified.beans.Annonce;
+import com.ecetech.bti4.itproject.classified.beans.User;
+import com.ecetech.bti4.itproject.classified.common.DBAction;
+
+public class AnnonceDAO {
+	public static ArrayList<Annonce> getAllAnnonce() throws SQLException {
+        DBAction.DBConnexion();
+        ArrayList<Annonce> annonces = new ArrayList<>();
+        String   req = ("SELECT * FROM Annonce'");
+        
+        try {
+            DBAction.setRes(DBAction.getStm().executeQuery(req));
+        } catch (SQLException ex) {
+        	System.out.println(ex.getErrorCode());
+        }
+
+        while (DBAction.getRes().next()) {
+        	Annonce annonce = (Annonce) DBAction.getRes();
+        	annonces.add(annonce);
+        
+        }
+
+        DBAction.DBClose();
+
+        return annonces;
+    }   
 }
