a50438ef34106cc1fc3215ace69bc084b3f8c600
Unknown <adrien.duhoux@gmail.com>
2016-12-03 19:19:05 +0100

    Modif Annonce DAO

--- Classified-Model/src/com/ecetech/bti4/itproject/classified/dao/AnnonceDAO.java
+++ Classified-Model/src/com/ecetech/bti4/itproject/classified/dao/AnnonceDAO.java
@@ -8,26 +8,27 @@ import com.ecetech.bti4.itproject.classified.beans.User;
 import com.ecetech.bti4.itproject.classified.common.DBAction;
 
 public class AnnonceDAO {
+
 	public static ArrayList<Annonce> getAllAnnonce() throws SQLException {
-        DBAction.DBConnexion();
-        ArrayList<Annonce> annonces = new ArrayList<>();
-        String   req = ("SELECT * FROM Annonce'");
-        
-        try {
-            DBAction.setRes(DBAction.getStm().executeQuery(req));
-        } catch (SQLException ex) {
-        	System.out.println(ex.getErrorCode());
-        }
+		DBAction.DBConnexion();
+		ArrayList<Annonce> annonces = new ArrayList<>();
+		String req = ("SELECT * FROM annonce;");
 
-        while (DBAction.getRes().next()) {
-        	Annonce annonce = (Annonce) DBAction.getRes();
-        	annonces.add(annonce);
-        	annonces.clear();
-        
-        }
+		try {
+			DBAction.setRes(DBAction.getStm().executeQuery(req));
+		} catch (SQLException ex) {
+			System.out.println("prout " + ex.getErrorCode());
+		}
+		int i = 0;
+		while (DBAction.getRes().next()) {
+			
+			Annonce annonce = (Annonce) DBAction.getRes();
+			annonces.add(annonce);
 
-        DBAction.DBClose();
+		}
 
-        return annonces;
-    }   
+		DBAction.DBClose();
+
+		return annonces;
+	}
 }
---
+++ Classified-Model/src/com/ecetech/bti4/itproject/classified/test/TestAnnonceDAO.java
@@ -0,0 +1,48 @@
+package com.ecetech.bti4.itproject.classified.test;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.ecetech.bti4.itproject.classified.beans.Annonce;
+import com.ecetech.bti4.itproject.classified.dao.AnnonceDAO;
+
+public class TestAnnonceDAO {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetAllAnnonce() {
+		ArrayList<Annonce> annonces = new ArrayList<>();
+		try {
+			annonces = AnnonceDAO.getAllAnnonce();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		affichArayList(annonces);
+
+	}
+
+	public void affichArayList(ArrayList<Annonce> annonces){
+		ListIterator<Annonce> list = annonces.listIterator();
+		while (list.hasNext()) {
+			System.out.println(list.next().toString());
+			
+		}
+	}
+	
+
+}
