9e54de453247880c593effe9b21c09f4dc9b2c0c
Unknown <adrien.duhoux@gmail.com>
2016-12-04 22:41:18 +0100

    Annonce DAO

--- Classified-Model/src/com/ecetech/bti4/itproject/classified/dao/AnnonceDAO.java
+++ Classified-Model/src/com/ecetech/bti4/itproject/classified/dao/AnnonceDAO.java
@@ -29,4 +29,25 @@ public class AnnonceDAO {
 
         return annonces;
 	}
+	
+	public static ArrayList<Annonce> getAnnonce() throws SQLException {
+		DBAction.DBConnexion();
+        ArrayList<Annonce> annonces = new ArrayList<>();
+        String req = ("SELECT * FROM annonce");
+        try {
+            DBAction.setRes(DBAction.getStm().executeQuery(req));
+        } catch (SQLException ex) {
+        	System.out.println("toto "+ex.getErrorCode());
+        }
+
+        while (DBAction.getRes().next()) {
+        	Annonce annonce = new Annonce(DBAction.getRes().getString(1),DBAction.getRes().getString(2),DBAction.getRes().getString(3),DBAction.getRes().getString(4),DBAction.getRes().getInt(5),DBAction.getRes().getDate(6),DBAction.getRes().getDate(7),DBAction.getRes().getInt(8),DBAction.getRes().getString(9),DBAction.getRes().getInt(10));
+        	annonces.add(annonce);
+        	
+        }
+
+        DBAction.DBClose();
+
+        return annonces;
+	}
 } 
