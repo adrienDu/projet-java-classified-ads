1730adfb0beed919f42c52ebef5e73d2342b55a5
MoazChaudhry <moaz.chaudry@hotmail.com>
2016-12-09 09:26:45 +0100

    PermissionDAO

--- Classified-Model/src/com/ecetech/bti4/itproject/classified/dao/PermissionDAO.java
+++ Classified-Model/src/com/ecetech/bti4/itproject/classified/dao/PermissionDAO.java
@@ -1,10 +1,41 @@
 package com.ecetech.bti4.itproject.classified.dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.ecetech.bti4.itproject.classified.beans.Annonce;
+import com.ecetech.bti4.itproject.classified.beans.Permission;
+import com.ecetech.bti4.itproject.classified.common.DBAction;
+
 public class PermissionDAO {
 
-	public PermissionDAO() {
-		// TODO Auto-generated constructor stub
-		System.out.println("test adrien");
-	}
+	public static ArrayList<Permission> getAllPermission() throws SQLException {
+        DBAction.DBConnexion();
+        ArrayList<Permission> permission = new ArrayList<>();
+        String   req = ("SELECT * FROM Permission'");
+        
+        try {
+            DBAction.setRes(DBAction.getStm().executeQuery(req));
+        } catch (SQLException ex) {
+        	System.out.println(ex.getErrorCode());
+        }
+
+        while (DBAction.getRes().next()) {
+        	Permission perm= (Permission) DBAction.getRes();
+        	permission.add(perm);
+        	permission.clear();
+        	
+        }
+
+        DBAction.DBClose();
+
+        return permission;
+    }   
+	
+	//public String hasPermission(){
+		// DBAction.DBConnexion();
+	    // ArrayList<Permission> permission = new ArrayList<>();
+	     //String   req = ("SELECT * FROM Permission WHERE permission = 1");
+	//}
 
 }
